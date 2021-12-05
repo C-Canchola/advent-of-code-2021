1ccab69b8e5b610591cd76b8493ed7eb9bd33e64
Caleb <c_canchola@icloud.com>
2021-12-05 00:19:13 +0000

    Changed iterator to not create a list

--- solution-day-1.py
+++ solution-day-1.py
@@ -2,12 +2,12 @@ def get_diff_iter():
     with open('input-day-1.txt') as f:
         prev = int(f.readline())
         cur = int(f.readline())
-        yield cur - prev
+        yield 1 if cur - prev > 0 else 0
 
         for l in f:
             prev = cur
             cur = int(l)
-            yield cur - prev
+            yield 1 if cur - prev > 0 else 0
 
 if __name__ == '__main__':
-    print(len([diff for diff in get_diff_iter() if diff > 0]))
+    print(sum(get_diff_iter()))
