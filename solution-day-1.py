901896bd6fa420eed6ed5d5c6562ca2bb7e2547f
Caleb <c_canchola@icloud.com>
2021-12-05 00:20:16 +0000

    Changed iterator to yield only on success

--- solution-day-1.py
+++ solution-day-1.py
@@ -2,12 +2,15 @@ def get_diff_iter():
     with open('input-day-1.txt') as f:
         prev = int(f.readline())
         cur = int(f.readline())
-        yield 1 if cur - prev > 0 else 0
+        if cur-prev > 0:
+            yield 1
+        
 
         for l in f:
             prev = cur
             cur = int(l)
-            yield 1 if cur - prev > 0 else 0
+            if cur - prev > 0:
+                yield 1
 
 if __name__ == '__main__':
     print(sum(get_diff_iter()))
