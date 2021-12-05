00c0a54de0a1711285e240935958d3afa5c5f8c0
Caleb <c_canchola@icloud.com>
2021-12-05 00:56:42 +0000

    Updated for part 2

--- solution-day-1.py
+++ solution-day-1.py
@@ -1,16 +1,24 @@
-def get_diff_iter():
-    with open('input-day-1.txt') as f:
-        prev = int(f.readline())
-        cur = int(f.readline())
-        if cur-prev > 0:
+'''made this an iterator cause I didn't know multiple reads were going to be necessary'''
+
+def get_diff_iter(file_name:str, group_count = 1):
+    with open(file_name) as f:
+
+        prev = [int(f.readline()) for _ in range(group_count)]
+        cur = prev[1:] + [int(f.readline())]
+
+        if sum(cur) - sum(prev) > 0:
             yield 1
         
-
         for l in f:
-            prev = cur
-            cur = int(l)
-            if cur - prev > 0:
-                yield 1
+            prev, cur = cur, cur[1:] + [int(l)]
+
+            if not sum(cur) - sum(prev) > 0:
+                continue
+            
+            yield 1
 
 if __name__ == '__main__':
-    print(sum(get_diff_iter()))
+    print(sum(get_diff_iter("ex-day-1.txt")))
+    print(sum(get_diff_iter("ex-day-1.txt", 3)))
+    print(sum(get_diff_iter("input-day-1.txt")))
+    print(sum(get_diff_iter("input-day-1.txt", 3)))
